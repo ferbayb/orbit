412ccfb56bfcf297d884925510559c8b5ee9d672
flintzb <flintbee@icloud.com>
2021-08-22 05:52:38 +1000

    Add user profiles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  def private_name
+    first_name + " " + last_name.slice(0) + "."
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'static_pages#landing'
+  get "signup" => "users#new"
   resources :tasks
+  devise_for :users
+  resources :users
 end
