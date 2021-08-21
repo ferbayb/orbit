3302713806bbd2c6b5f26ba4b33bceaa25945123
flintzb <flintbee@icloud.com>
2021-08-21 16:18:42 +1000

    Add Geocoding Functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -35,8 +35,7 @@ class TasksController < ApplicationController
     end
 
     private
-
-    def task_params
-        params.require(:task).permit(:address, :description)
-    end
+        def task_params
+            params.require(:task).permit(:address, :description, :latitude, :longitude)
+        end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ApplicationRecord
+    validates :address, presence: true
+
+    def map_embed(lat, lon)
+        "https://www.google.com/maps/embed/v1/place?q=#{lat}%2C#{lon}&key=AIzaSyBWcv3SgzJkW2biuy0ii1Tjf-EhCNzhYz8"
+    end
 end
