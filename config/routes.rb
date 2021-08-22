dcbb81a55cefe30fdb54e818705c0128b30be5ad
flintzb <flintbee@icloud.com>
2021-08-22 18:13:22 +1000

    Install invisible_captcha and fix routing errors in header

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   root 'static_pages#landing'
   get "signup" => "users#new"
   resources :tasks
-  devise_for :users
   resources :users
 end
