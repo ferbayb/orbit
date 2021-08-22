0815b0b579a9ce1c435642c4c60f3ef6fcfb7019
flintzb <flintbee@icloud.com>
2021-08-22 16:02:59 +1000

    Add connections to appropriate views for tasks and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  has_many :posts
-  
+  has_many :tasks, dependent: :destroy
+
   def private_name
     first_name + " " + last_name.slice(0) + "."
+  end
+
+  def age
+    return nil unless birthday.present?
+    today = Time.current.to_date
+      if today.month < birthday.month || (today.month == birthday.month && birthday.day > today.day)
+        today.year - birthday.year - 1
+      else
+        today.year - birthday.year
+      end
   end
 end
