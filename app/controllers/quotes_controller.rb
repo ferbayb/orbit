e4396c0529c8887c0eaf9e83171f7b8d19a923c4
flintzb <flintbee@icloud.com>
2021-08-24 03:23:38 +1000

    Add fully functional and styled quoting system

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,40 +1,50 @@
 class QuotesController < ApplicationController
-    before_action :find_task
-    before_action :find_quote, only: [:destroy]
+  before_action :find_task
+  before_action :find_quote, only: [:destroy, :edit, :update, :quote_owner]
+  before_action :quote_owner, only: [:destroy, :edit, :update]
 
-    def create
-        @quote = @task.quotes.create(params[:quote].permit(:content, :price, :booking))
-        @quote.user_id = current_user.id
-        @quote.save
+  def create
+    @quote = @task.quotes.create(params[:quote].permit(:content, :price, :booking))
+    @quote.user_id = current_user.id
+    @quote.save
 
-        if @quote.save
-            redirect_to task_path(@task)
-        else
-            render :new
-        end
+    if @quote.save
+      redirect_to task_path(@task)
+    else
+      render :new
     end
+  end
 
-    def update
-        if @quote.update(params[:quote].permit(:content, :price, :booking))
-            redirect_to task_path(@task), notice: "Success."
-        else
-            redirect_to task_path(@task), alert: "Quote not saved."
-        end
+  def edit
+    @task = Task.find(params[:task_id])
+  end
+
+  def update
+    if @quote.update(params[:quote].permit(:content, :price, :booking))
+      redirect_to task_path(@task), notice: "Success."
+    else
+      redirect_to task_path(@task), alert: "Quote not saved."
     end
+  end
 
-    def destroy
-        @quote.destroy
-        redirect_to task_path(@task)
+  def destroy
+    @quote.destroy
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def find_task
+    @task = Task.find(params[:task_id])
+  end
+
+  def find_quote
+    @quote = @task.quotes.find(params[:id])
+  end
+
+  def quote_owner
+    unless current_user.id == @quote.user_id || current_user.id == @task.user_id
+        flash[:alert] = "You are not the owner of this quote or task."
     end
-
-
-    private
-
-        def find_task
-            @task = Task.find(params[:task_id])
-        end
-        
-        def find_quote
-            @quote = @task.quotes.find(params[:id])
-        end
+  end
 end
